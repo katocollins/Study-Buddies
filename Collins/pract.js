bbb92964f7d5d71a6e4b53aebee37b3669a22584
Kato Collins <ckato131@gmail.com>
2024-02-18 16:13:18 +0300

    Added test file

---
+++ Collins/pract.js
@@ -0,0 +1,19 @@
+var ame = `Collins`;
+
+console.log(ame);
+
+function fund(p, r) {
+    sum = p + r;
+
+    return sum;
+}
+
+console.log(fund(4, 3));
+
+//Arrow Functions
+
+var ken = (k, f) => {
+    return mult = k * f;
+}
+
+console.log(ken(4, 5));
