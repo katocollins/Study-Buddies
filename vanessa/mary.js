d1353495d255061e90d29904704f9b0df5be41f1
Nansumbamaryvanessa <108078418+mary-nessa@users.noreply.github.com>
2024-02-21 01:38:58 -0800

    new code to vanessa

--- vanessa/mary.js
+++ vanessa/mary.js
@@ -1,35 +1,58 @@
-for(let number=1; number<=7;number++){
-    let tag="";
-    for(let j=0;j<number;j++){
-        tag+="#";
-    }
-    console.log(tag);
-}
+// for(let number=1; number<=7;number++){
+//     let tag="";
+//     for(let j=0;j<number;j++){
+//         tag+="#";
+//     }
+//     console.log(tag);
+// }
 
-for(let input=1;input<=100;input++){
-    if (input % 3 === 0 && input % 5 === 0) {
-        console.log("FizzBuzz");
-    } else if (input % 3 === 0) {
-        console.log("Fizz");
-    } else if (input % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(input);
-    }
+// for(let input=1;input<=100;input++){
+//     if (input % 3 === 0 && input % 5 === 0) {
+//         console.log("FizzBuzz");
+//     } else if (input % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (input % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(input);
+//     }
        
-}
-let size = 8;
-let board = "";
+// }
+// let size = 8;
+// let board = "";
 
-for (let row = 0; row < size; row++) {
-  for (let col = 0; col < size; col++) {
-    if ((row + col) % 2 === 0) {
-      board += " "; // Place a space for even-indexed cells
-    } else {
-      board += "#"; // Place a '#' for odd-indexed cells
-    }
+// for (let row = 0; row < size; row++) {
+//   for (let col = 0; col < size; col++) {
+//     if ((row + col) % 2 === 0) {
+//       board += " "; // Place a space for even-indexed cells
+//     } else {
+//       board += "#"; // Place a '#' for odd-indexed cells
+//     }
+//   }
+//   board += "\n"; // Add a newline character after each row
+// }
+
+// console.log(board);
+
+
+function min(a,b){
+  if(a>b){
+    // return b;
+    return a- b;
+  }else{
+    // return a;
+    return b- a;
   }
-  board += "\n"; // Add a newline character after each row
 }
+console.log(min(5,110));
 
-console.log(board);
+function isEven( wholenumber){
+  if(number%2===0){
+    return true;
+  }else if(!number%2===0){
+    return false;
+  }else{
+    return number -2;
+  }
+}
+console.log(isEven(15));
